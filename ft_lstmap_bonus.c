8cdb7c2802a7d522411f0a6cfe9e224aa9a67be2
namejojo <jlima-so@42lisboa.com>
2025-04-12 11:43:23 +0100

    msg

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 17:29:20 by jlima-so          #+#    #+#             */
-/*   Updated: 2025/04/11 14:27:57 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/04/12 11:39:26 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,23 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*ret;
+	t_list	*temp;
 
-	ret = lst;
-	while (lst->next != NULL)
+	if (lst == NULL)
+		return (NULL);	
+	ret = ft_lstnew(f(lst->content));
+	if (ret == NULL)
+		return (NULL);
+	lst = lst->next;
+	while (lst != NULL)
 	{
-		if (lst->content != NULL)
-			del(lst->content);
-		f (lst->content);
+		temp = ft_lstnew(f(lst->content));
+		if (temp == NULL)
+		{
+			ft_lstclear (&ret, del);
+			return (NULL);				
+		}
+		ft_lstadd_back(&ret, temp);
 		lst = lst->next;
 	}
 	return (ret);
