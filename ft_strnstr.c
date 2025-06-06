7692024db12261804accfd1100d96c6c984f6bda
namejojo <jlima-so@42lisboa.com>
2025-06-06 13:52:45 +0100

    msg

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: jlima-so <jlima-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:28:40 by jlima-so          #+#    #+#             */
-/*   Updated: 2025/04/17 13:02:24 by jlima-so         ###   ########.fr       */
+/*   Updated: 2025/06/06 13:50:48 by jlima-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little)
 {
 	size_t	indl;
 	size_t	indb;
@@ -20,12 +20,10 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	indb = 0;
 	if (*little == '\0')
 		return ((char *)big);
-	if (len == 0)
-		return (NULL);
-	while (big[indb] && indb < len)
+	while (big[indb])
 	{
 		indl = 0;
-		while (big[indb + indl] == little[indl] && indb + indl < len)
+		while (big[indb + indl] == little[indl])
 		{
 			indl++;
 			if (little[indl] == '\0')
